38060fee4a847d2ee2f284e0d5fed15fa4c137c4
filipchelkowski <filip.chelkowski@gmail.com>
2026-03-14 19:25:41 +0100

    Update Injector.java

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -21,28 +20,30 @@ public class Injector {
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        Object clazzImplementationInstance = null;
         Class<?> clazz = findImplementation(interfaceClazz);
-        if (clazz.isAnnotationPresent(Component.class)) {
-            Field[] declaredFields = clazz.getDeclaredFields();
-            for (Field field : declaredFields) {
-                if (field.isAnnotationPresent(Inject.class)) {
-                    // create an object of a field type
-                    Object fieldInstance = getInstance(field.getType());
-                    // create an object of an interfaceClazz or an implementation class
-                    clazzImplementationInstance = createNewInstance(clazz);
-                    // set 'field type object' to 'interfaceClazz object'
-                    field.setAccessible(true);
-                    try {
-                        field.set(clazzImplementationInstance, fieldInstance);
-                    } catch (IllegalAccessException e) {
-                        throw new RuntimeException("Can't initialize field value. Class "
-                                + clazz.getName() + ". Field: " + field.getName());
-                    }
+        if (clazz == null || !clazz.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException("No implementation found for "
+                    + interfaceClazz.getName());
+        }
+
+        Object clazzImplementationInstance = createNewInstance(clazz);
+
+        Field[] declaredFields = clazz.getDeclaredFields();
+        for (Field field : declaredFields) {
+            if (field.isAnnotationPresent(Inject.class)) {
+                // create an object of a field type
+                Object fieldInstance = getInstance(field.getType());
+                // create an object of an interfaceClazz or an implementation class
+                // clazzImplementationInstance = createNewInstance(clazz);
+                // set 'field type object' to 'interfaceClazz object'
+                field.setAccessible(true);
+                try {
+                    field.set(clazzImplementationInstance, fieldInstance);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Can't initialize field value. Class "
+                            + clazz.getName() + ". Field: " + field.getName(), e);
                 }
             }
-        } else {
-            throw new RuntimeException("Can't create an instance");
         }
 
         if (clazzImplementationInstance == null) {
@@ -63,10 +64,9 @@ public class Injector {
             Object object = constructor.newInstance();
             instances.put(clazz, object);
             return object;
-        } catch (NoSuchMethodException | InvocationTargetException
-                 | InstantiationException | IllegalAccessException e) {
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can't create a news instance of "
-                    + clazz.getName(), e);
+                    + clazz.getName());
         }
     }
 
@@ -77,9 +77,17 @@ public class Injector {
         implementationMap.put(ProductService.class, ProductServiceImpl.class);
 
         if (interfaceClazz.isInterface()) {
-            return implementationMap.get(interfaceClazz);
+            Class<?> implementation = implementationMap.get(interfaceClazz);
+
+            if (implementation == null) {
+                throw new RuntimeException("No implementation found for "
+                        + interfaceClazz.getName());
+            }
+
+            return implementation;
         }
-        
+
         return interfaceClazz;
     }
+
 }
